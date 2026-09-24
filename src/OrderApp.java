eaa4d492742c39dbfa64a13ce21a29b29227e539
Kéven <keven.taurelle@gmail.com>
2026-09-24 11:53:59 +0200

    Refactor duplicated code using methods (DRY)

--- src/OrderApp.java
+++ src/OrderApp.java
@@ -1,24 +1,28 @@
 public class OrderApp {
+
+    static double computeTotalWithVat(double unitPrice, int quantity) {
+        return unitPrice * quantity + unitPrice * quantity * 0.2;
+    }
+
+    static void printRecu( String nom, double prix, int quantite, double tot){
+        System.out.println("Client : " + nom);
+        System.out.println("Prix HT : " + prix);
+        System.out.println("Quantité : " + quantite);
+        System.out.println("Total TTC : " + tot);
+        System.out.println("------------------------");
+    }
+    
     public static void main(String[] args) {
         String firstCustomerName = "Alice";
         double firstUnitPrice = 10;
         int firstQuantity = 2;
-        double firstTotalWithVal = firstUnitPrice * firstQuantity + firstQuantity * firstUnitPrice * 0.2;
+        double firstTotalWithVal = computeTotalWithVat(firstUnitPrice, firstQuantity);
         String secondCustomerName = "Bob";
         double secondUnitPrice = 15;
         int secondQuantity = 1;
-        double secondTotalWithVal = secondUnitPrice * secondQuantity + secondUnitPrice * secondQuantity * 0.2;
-        System.out.println("Client : " + firstCustomerName);
-        System.out.println("Prix HT : " + firstUnitPrice);
-        System.out.println("Quantité : " + firstQuantity);
-        System.out.println("Total TTC : " + firstTotalWithVal);
-        System.out.println("------------------------");
-        System.out.println("Client : " + secondCustomerName);
-        System.out.println("Prix HT : " + secondUnitPrice);
-
-        System.out.println("Quantité : " + secondQuantity); 
-        System.out.println("Total TTC : " + secondTotalWithVal);
-        System.out.println("------------------------");
+        double secondTotalWithVal = computeTotalWithVat(secondUnitPrice, secondQuantity);
+        printRecu(firstCustomerName, firstUnitPrice, firstQuantity,  firstTotalWithVal);
+        printRecu(secondCustomerName, secondUnitPrice, secondQuantity, secondTotalWithVal);
         if (firstTotalWithVal + secondTotalWithVal > 30) {
             System.out.println("Réduction !");
         } else {
