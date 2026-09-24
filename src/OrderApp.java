46c6b99fe3a84f8e5ae75b22c06bc198fdb2daa2
Kéven <keven.taurelle@gmail.com>
2026-09-24 11:58:23 +0200

    Refactor duplicated code using methods (DRY)

--- src/OrderApp.java
+++ src/OrderApp.java
@@ -4,25 +4,31 @@ public class OrderApp {
         return unitPrice * quantity + unitPrice * quantity * 0.2;
     }
 
-    static void printRecu( String nom, double prix, int quantite, double tot){
+    static double printRecu( String nom, double prix, int quantite){
+
+        double tot = computeTotalWithVat(prix, quantite);
         System.out.println("Client : " + nom);
         System.out.println("Prix HT : " + prix);
         System.out.println("Quantité : " + quantite);
         System.out.println("Total TTC : " + tot);
         System.out.println("------------------------");
+
+        return tot;
     }
+
     
     public static void main(String[] args) {
         String firstCustomerName = "Alice";
         double firstUnitPrice = 10;
         int firstQuantity = 2;
-        double firstTotalWithVal = computeTotalWithVat(firstUnitPrice, firstQuantity);
+        
         String secondCustomerName = "Bob";
         double secondUnitPrice = 15;
         int secondQuantity = 1;
-        double secondTotalWithVal = computeTotalWithVat(secondUnitPrice, secondQuantity);
-        printRecu(firstCustomerName, firstUnitPrice, firstQuantity,  firstTotalWithVal);
-        printRecu(secondCustomerName, secondUnitPrice, secondQuantity, secondTotalWithVal);
+
+        double firstTotalWithVal = printRecu(firstCustomerName, firstUnitPrice, firstQuantity);
+        double secondTotalWithVal = printRecu(secondCustomerName, secondUnitPrice, secondQuantity);
+        
         if (firstTotalWithVal + secondTotalWithVal > 30) {
             System.out.println("Réduction !");
         } else {
